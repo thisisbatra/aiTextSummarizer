851ec61ffc695afcc8432367fc9cc910864a9cf7
Ajay <ajgour14@gmail.com>
2024-04-08 14:03:43 +0530

    learning dom manipulation

---
+++ logic.js
@@ -0,0 +1,11 @@
+// // document.getElementById("inputArea").innerHTML = "Hello World!";?\
+// function click() {
+//     var BUTTON = document.getElementById("inputArea").value;
+//     document.getElementById("sudo").innerHTML = GFG;
+
+// }
+const onSubmit=()=>{
+    let stringVal=document.getElementById("inputArea").value;
+        // document.getElementById("outputArea").innerHTML=stringVal;
+        console.log(stringVal.length)
+}
